09b3a60ee01dc1c19c07ac3d965b0e926b43a91d
Bunny <w@bunnywong.com>
2015-07-06 02:09:14 +0800

    [ profile ] [ dashboard ] add  template

---
+++ www/wp-content/themes/the-newswire/my-cusotmer-dashboard.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: My Dashboard
+ * Description: Custom dashboard in full-width
+ */
+get_header(); ?>
+
+    <div id="content" class="clearfix full-width-content">
+
+        <div id="main" class="clearfix" role="main">
+todo: block client role to visit other user
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<a href="/profile">My Profile</a>
+
+				<?php comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</div>
+		</div>
+
+<?php get_footer(); ?>
---
+++ www/wp-content/themes/the-newswire/my-cusotmer-profile.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Profile
+ * Description: Profile in full-width
+ */
+get_header(); ?>
+
+<style>
+	label {
+    text-transform: capitalize!important;
+		margin: 0px!important;
+	}
+	.mycred-my-balance-wrapper {
+		font-size: 40px;
+	}
+</style>
+
+    <div id="content" class="clearfix full-width-content">
+
+        <div id="main" class="clearfix" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<a href="/profile">My Profile</a>
+
+				<?php comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</div>
+		</div>
+
+<?php get_footer(); ?>
+
+<script>
+jQuery(document).ready(function($){
+
+	$('.mycred-my-balance-wrapper > div').prepend('My Balance: ')
+});
+</script>
