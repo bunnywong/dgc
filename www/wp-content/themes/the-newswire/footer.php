7ee8889151557c84a3fb15018ae421bf4a367dc8
BunnY <w@bunnywong.com>
2015-10-21 21:06:38 +0800

    front-end style

--- www/wp-content/themes/the-newswire/footer.php
+++ www/wp-content/themes/the-newswire/footer.php
@@ -11,8 +11,9 @@
 			<?php echo __('&copy; ', 'newswire') . esc_attr( get_bloginfo( 'name', 'display' ) );  ?>
             <?php if ( is_front_page() && ! is_paged() ) : ?>
               <br>
-            <?php _e('Powered by ', 'newswire'); ?><a href="<?php echo esc_url( __( 'http://wordpress.org/', 'newswire' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'newswire' ); ?>"><?php _e('WordPress' ,'newswire'); ?></a>
+            <span class="no-print"><?php _e('Powered by ', 'newswire'); ?><a href="<?php echo esc_url( __( 'http://wordpress.org/', 'newswire' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'newswire' ); ?>"><?php _e('WordPress' ,'newswire'); ?></a>
 			<?php _e(' and ', 'newswire'); ?><a href="<?php echo esc_url( __( 'http://wpdevshed.com/', 'newswire' ) ); ?>"><?php _e('WP Dev Shed', 'newswire'); ?></a>
+            </span>
             <?php endif; ?>
 
 		</div>
