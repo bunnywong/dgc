1f2b5a5a8271579b9342998dca31458565aa055e
BunnY <w@bunnywong.com>
2015-10-21 15:26:52 +0800

    change copyright

--- www/wp-content/themes/the-newswire/footer.php
+++ www/wp-content/themes/the-newswire/footer.php
@@ -10,7 +10,8 @@
 
 			<?php echo __('&copy; ', 'newswire') . esc_attr( get_bloginfo( 'name', 'display' ) );  ?>
             <?php if ( is_front_page() && ! is_paged() ) : ?>
-            <?php //_e('- Powered by ', 'newswire'); ?><a href="<?php echo esc_url( __( 'http://wordpress.org/', 'newswire' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'newswire' ); ?>"><?php _e('WordPress' ,'newswire'); ?></a>
+              <br>
+            <?php _e('Powered by ', 'newswire'); ?><a href="<?php echo esc_url( __( 'http://wordpress.org/', 'newswire' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'newswire' ); ?>"><?php _e('WordPress' ,'newswire'); ?></a>
 			<?php _e(' and ', 'newswire'); ?><a href="<?php echo esc_url( __( 'http://wpdevshed.com/', 'newswire' ) ); ?>"><?php _e('WP Dev Shed', 'newswire'); ?></a>
             <?php endif; ?>
 
